f51a818ed4224ed1d0b04db78a375ff59f5c0327
D44F <dumar1375@gmail.com>
2026-05-12 12:49:01 -0500

    Proyecto inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,12 @@
+
 plugins {
     alias(libs.plugins.android.application)
 }
 
 android {
+    buildFeatures {
+        viewBinding = true
+    }
     namespace = "com.android.nodocivico"
     compileSdk {
         version = release(36) {
@@ -44,4 +48,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.material:material:1.11.0")
+
+
 }
